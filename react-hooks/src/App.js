808045c81a1b947561770024ccd9dd2d9cef1864
Rawat <divyanshu.r46956@gmail.com>
2019-11-06 13:13:22 +0100

    A project to make use of react-hooks.

---
+++ react-hooks/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+/**
+ * In short, you can now use state, and other React features, within your functional React components.
+ *  So, you don’t need a class to use state!
+ */
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
