54b991781af6ad2eea08ee7c51458924f967d2fd
Rawat <divyanshu.r46956@gmail.com>
2019-11-06 14:48:36 +0100

    Added atnd to the project.

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Input } from 'antd';
+import 'antd/es/input/style/index.css';
 import './App.css';
 
 
 /**
  * In short, you can now use state, and other React features, within your functional React components.
  *  So, you don’t need a class to use state!
+ * The biggest part to learn about hooks is the new way of declaring state. So as the first part of this app, let’s set up a component with state, the hooks way.
  */
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-
+    <div className="container">
+      <h2>Type something...</h2>
+      <Input placeholder="Basic usage"/>
     </div>
-  );
+  )
 }
-
 export default App;
